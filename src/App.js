a864a437f8b00d4bc01c0a73a286755ae3c2b90f
Eric Hoffman <eric.peter.hoffman@gmail.com>
2024-01-09 18:51:22 -0700

    Styled Home, still need to fix it for smaller screens.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <div className='App'>
           <Navbar/>
-          <Header/>
+          {/* <Header/> */}
 
           <Home/>
           <WID/>
--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import baseImageHome from '../../assets/baseImageHome.svg'
+import baseEllipseHome from '../../assets/baseEllipseHome.svg'
+import portrait from '../../assets/portrait.png'
 import './home.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='eh__home section__padding' id='home'>
+      <div className='gradient__bar'></div>
+      
+      <div className='eh__home-content'>
+
+
+        <h1 className='gradient__text'>Welcome</h1>
+
+        <div className='eh__home-content_subtext'>
+          <p className='subtext-larger'>Full Stack Web Development</p>
+          <p className='subtext-larger' id='final-subtext-larger'>Front End Design</p>
+          <p>Creating applications from start to finish with clean and efficient design.</p>
+        </div>
+
+        <div className='eh__home-image'>
+          <img className='rotate-center' src={baseImageHome} alt='placeholder'/>
+          <img className='slide-left' src={baseEllipseHome} alt='placeholder'/>
+          <img className='portrait' src={portrait} alt='placeholder'/>
+        </div>
+
+
+      </div>
     </div>
   )
 }
