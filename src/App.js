2ac5bb9492369530a6fd34c8d76ca1bb3c7e746a
Eric Hoffman <eric.peter.hoffman@gmail.com>
2024-01-16 23:14:30 -0700

    Started working on animation to slide component off the page and load a new one on a button click. Mostly working except the animation for sliding the component in.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Header, Home, WID, Contact, Footer } from './containers';
+import React, { useState } from 'react';
+import { Header, Home, WID, Contact, Footer, Projects } from './containers';
 import { Navbar, Message } from './components';
 import './app.css'
 
 const App = () => {
+  const [showProj, setShowProj] = useState(false)
+
+  const projBtn = () => {
+    setShowProj(true);
+  }
+  
   return (
     <div className='App'>
           <Navbar/>
           
           <Home/>
-          {/* <Header/> */}
+          {/* <div className={`header ${showProj ? 'slide-out' : ''}`}>
+            <Header
+              handleClick={projBtn}
+            />
+          </div> */}
+          {/* {showProj && <Projects className={`projects ${showProj ? 'slide-in' : ''}`}/>} */}
           <WID/>
           {/* <Contact/> */}
 
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,30 +1,15 @@
 import React from 'react'
-import widImage from '../../assets/widImage.svg'
 import './header.css'
 
-const Header = () => {
+const Header = ({ handleClick }) => {
   return (
-    <div className='container section__padding' id='wid'>
-      <div className='fi-1'>
-        <div className='image'>
-          <img src={widImage} alt='placeholder'/>
-        </div>
+    <div
+      className='container section__padding'>
+      <div className='btn'>
+        <button type='button' onClick={handleClick}>Click Me</button>
       </div>
-      <div className='fi-2'>
-        <div className='text'>
-          <h1>What I Do</h1>
-          <p>I create full stack web applications
-            and sites using JavaScript, HTML, CSS,
-            and frameworks such as React, NodeJS,
-            and PostgreSQL.<br/>
-            Interested in seeing some of my work?
-            Click the button to see more!
-          </p>
-          <div className='btn'>
-            <button type='button'>My Work</button>
-          </div>
-        </div>
-        <div className='bar'></div>
+      <div className='text'>
+        <p>This should go away.</p>
       </div>
     </div>
   )
