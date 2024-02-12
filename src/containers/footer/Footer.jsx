6ec97d015a30efa0e9beda9096134db56b52be57
Eric Hoffman <eric.peter.hoffman@gmail.com>
2024-02-12 16:31:26 -0700

    Updated Resume Link

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
         <div className='eh__footer-links'>
           <p><a href='https://www.linkedin.com/in/erichoffman98/' target='blank'>LinkedIn</a></p>
           <p><a href='https://github.com/EricTheHoff' target='blank'>Github</a></p>
-          <p><a href='https://drive.google.com/file/d/1aoFr82uaBhfC5eLwWI1VFTM-U3XY9vEW/view?usp=sharing' target='blank'>View My Resume</a></p>
+          <p><a href='https://drive.google.com/file/d/1O1BLCSYrX2_F9oWlatzrO8zS2GaQzYuK/view?usp=sharing' target='blank'>View My Resume</a></p>
         </div>
       </div>
 
